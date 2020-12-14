c7c97481295259fa7e90ccb4335aa17c668de195
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2020-12-14 10:31:55 +0100

    user model done

---
+++ bin/seedUser.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+require("./../config/mongo");
+const UserModel = require("./../models/Users");
+
+const users = [
+  {
+    username: "Jean-Claude",
+    email: "jcvd@mail.com",
+    password: "1234",
+    avatar: "",
+  },
+];
+
+async function inserUser() {
+  try {
+    await UserModel.deleteMany(); // empty the styles db collection
+    const inserted = await UserModel.insertMany(users); // insert docs in db
+    console.log(`seed labels done : ${inserted.length} documents inserted !`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+inserUser();
